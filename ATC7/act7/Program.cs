109a0cd7362b4343738ee52b063352f5fe811720
La Polola <polola135@gmail.com>
2022-05-25 12:37:35 -0300

    agregue carpetas

---
+++ ATC7/act7/Program.cs
@@ -0,0 +1,44 @@
+﻿//act 7 Osone Paula
+//defino el array e ingresa los valores
+int[] numeros = new int[10];
+Console.WriteLine("Ingresa 10 número para generar la lista");
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    numeros[i] = int.Parse(Console.ReadLine());
+}
+
+//punto 2 suma todo
+
+int aux = 0;
+
+for (int i = 0; i < numeros.Length; i++)
+{
+     aux = aux + numeros[i];
+}
+
+//Console.WriteLine("La suma total es " + aux);
+
+//punto 3 maximo y 4 minimo
+var mayorValor = numeros.Max();
+var menorValor = numeros.Min();
+
+//Console.WriteLine("El mayor de todos los números es " + mayorValor);
+// Console.WriteLine("El menor de todos los números es " + menorValor);
+
+//5 promedio
+
+decimal promedio = Decimal.Divide(aux, numeros.Length);
+
+//Console.WriteLine("Promedio: " + promedio);
+
+// act 6 mostrar todo
+
+Console.WriteLine("Los números ingresados fueron");
+
+for (int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine(numeros[i]);
+}
+
+// Respecto al punto 7 en cada ejercicio dejé las impresiones respectivas. 
---
+++ actividadClase5/PoloT5/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//ejercicio clase 5 osone paula andrea
+
+string respuesta = "S";
+
+while (respuesta.ToUpper() == "S")
+{
+    Console.Clear();
+
+    Console.WriteLine("¿Cuál es tu nombre?");
+    string nombre = Console.ReadLine();
+    Console.WriteLine($"¡Hola {nombre}!");
+
+    Console.WriteLine("¿Desea continuar? Ingresa S/N");
+    respuesta = Console.ReadLine();
+}
+
+if (respuesta.ToUpper() == "N")
+{
+    Console.WriteLine("Programa finalizado correctamente");
+} else
+{
+    Console.WriteLine("Opción no válida");
+}
